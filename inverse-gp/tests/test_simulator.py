6ec13203f8c23c4a1457336ef810851cb7be3aaf
Samuel Murray <samuelmu@kth.se>
2019-02-08 16:50:47 +0100

    Fix: Update test to reflect changes in Simulator - Simulator now takes Tensor as input

--- inverse-gp/tests/test_simulator.py
+++ inverse-gp/tests/test_simulator.py
@@ -1,11 +1,13 @@
 import unittest
 
+import torch
+
 from simulator import Simulator
 
 
 class TestSimulator(unittest.TestCase):
     def test_call(self):
         simulator = Simulator()
-        x = 5.
+        x = torch.linspace(0, 1, 20)
         y = simulator(x)
-        self.assertEqual(x, y)
+        self.assertEqual(x.shape, y.shape)
