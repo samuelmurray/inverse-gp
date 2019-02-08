8b634e3057f3b50d31ab4dff756daa7330a45102
Samuel Murray <samuelmu@kth.se>
2019-02-08 16:28:40 +0100

    Feature: Add arbitrary function as simulator

--- inverse-gp/simulator.py
+++ inverse-gp/simulator.py
@@ -1,6 +1,10 @@
+import torch
+import numpy as np
+
+
 class Simulator:
     def __init__(self):
         pass
 
     def __call__(self, x):
-        return x
+        return torch.sin(x * (2 * np.pi)) + torch.randn(x.size()) * 0.2
