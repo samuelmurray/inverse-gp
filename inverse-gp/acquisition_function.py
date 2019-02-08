32265a761b3bea65f84537511d8a90f1f46783b5
Samuel Murray <samuelmu@kth.se>
2019-02-08 17:32:36 +0100

    Refactor: Rename class - acquisition function better reflects purpose

--- inverse-gp/acquisition_function.py
+++ inverse-gp/acquisition_function.py
@@ -1,11 +1,12 @@
 import gpytorch
 import torch
 from torch.distributions.normal import Normal
+import numpy as np
 
 from gp import GP
 
 
-class Optimizer(gpytorch.Module):
+class AcquisitionFunction(gpytorch.Module):
     def __init__(self, model: GP, simulator):
         super().__init__()
         self.model = model
@@ -15,17 +16,22 @@ class Optimizer(gpytorch.Module):
     def forward(self, x, y, candidate_set):
         self.model.eval()
         self.model.likelihood.eval()
-        self.model.train_inputs = x
-        self.model.train_targets = y
+        #self.model.train_inputs = x
+        #self.model.set_train_data(x, y)
+        model = self.model.get_fantasy_model(x, y)
+        #self.model.train_targets = y
 
-        pred = self.gp_model.likelihood(self.model(candidate_set))
+        pred = self.model.likelihood(self.model(candidate_set))
 
-        mu = pred.mean().detach()
-        sigma = pred.std().detach()
+        mu = pred.mean.detach()
+        var = pred.variance.detach()
+        std = np.sqrt(var)
 
-        u = (self.best_y - mu) / sigma
+        best_y = torch.max(y)
+
+        u = (best_y - mu) / std
         m = Normal(torch.Tensor([0.0]), torch.Tensor([1.0]))
         ucdf = m.cdf(u)
         updf = torch.exp(m.log_prob(u))
-        expected_improvement = sigma * (updf + u * ucdf)
+        expected_improvement = var * (updf + u * ucdf)
         return expected_improvement
