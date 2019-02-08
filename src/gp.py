a08e05a12983190713d0d5db7d5a60b751c3e4cb
Samuel Murray <samuelmu@kth.se>
2019-02-08 13:20:14 +0100

    Refactorize: Rename variable, covar_* -> covariance_*

--- src/gp.py
+++ src/gp.py
@@ -5,9 +5,9 @@ class GP(gpytorch.models.ExactGP):
     def __init__(self, likelihood):
         super(GP, self).__init__(None, None, likelihood)
         self.mean_module = gpytorch.means.ConstantMean()
-        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+        self.covariance_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
 
     def forward(self, x):
         mean_x = self.mean_module(x)
-        covar_x = self.covar_module(x)
-        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+        covariance_x = self.covariance_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covariance_x)
