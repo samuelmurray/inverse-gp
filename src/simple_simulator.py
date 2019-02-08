035e33ca4f786f1263fc5e20bbd9e4aa4e081e6c
Samuel Murray <samuelmu@kth.se>
2019-02-08 11:45:31 +0100

    Initial commit

---
+++ src/simple_simulator.py
@@ -0,0 +1,6 @@
+class SimpleSimulator:
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        return x
---
+++ src/simulator.py
@@ -0,0 +1,6 @@
+class Simulator:
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        return x
