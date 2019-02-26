e1b7d5539fddddb12d48c1d15afc4c06dfa19812
Samuel Murray <samuelmu@kth.se>
2019-02-26 17:28:11 +0100

    Feature: Generate outputs with simulator

--- invgp/tests/test_gp.py
+++ invgp/tests/test_gp.py
@@ -5,13 +5,15 @@ import numpy as np
 import torch
 
 from invgp.model import GP
+from invgp.simulator import SimpleSimulator
 
 
 class TestExpectedImprovement(unittest.TestCase):
     def setUp(self) -> None:
         np.random.seed(1534315123)
         self.x = torch.Tensor(np.random.normal(size=[10, 2]))
-        self.y = self.x.sin()
+        simulator = SimpleSimulator()
+        self.y = simulator(self.x)
         self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
         self.model = GP(self.x, self.y, self.likelihood)
 
