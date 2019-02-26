2c9e7e734d6a198a46feb52f1344d71467808e53
Samuel Murray <samuelmu@kth.se>
2019-02-26 17:57:23 +0100

    Refactor: Rename variable input to input_train

--- invgp/tests/test_gp.py
+++ invgp/tests/test_gp.py
@@ -14,10 +14,10 @@ class TestExpectedImprovement(unittest.TestCase):
         simple_simulator = SimpleSimulator()
         heavy_simulator = HeavySimulator()
         self.x = torch.Tensor(np.random.normal(size=[10, 2]))
-        self.input = simple_simulator(self.x)
+        self.input_train = simple_simulator(self.x)
         self.y = heavy_simulator(self.x)
         self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self.model = GP(self.input, self.y, self.likelihood)
+        self.model = GP(self.input_train, self.y, self.likelihood)
 
     def test_get_inputs(self) -> None:
         model_inputs = self.model.get_inputs()
