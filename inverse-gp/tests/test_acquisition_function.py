a336b8405a1a5cb27e341964ee6b1a5c19d1464c
Samuel Murray <samuelmu@kth.se>
2019-02-26 14:07:49 +0100

    Initial commit

---
+++ inverse-gp/tests/test_acquisition_function.py
@@ -0,0 +1,26 @@
+import unittest
+
+import gpytorch
+import numpy as np
+import torch
+
+from acquisition_functions import ExpectedImprovement
+from simulators import SimpleSimulator
+from gp import GP
+
+
+class TestExpectedImprovement(unittest.TestCase):
+    def test_call(self) -> None:
+        simulator = SimpleSimulator()
+        x = torch.linspace(0, 1, 20)
+        y = simulator(x)
+        likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        model = GP(x, y, likelihood)
+        acquisition_function = ExpectedImprovement(model, simulator)
+        candidate_set = torch.linspace(-1, 2, 100)
+        expected_improvement = acquisition_function(x, y, candidate_set)
+        self.assertEqual(np.array([100]), expected_improvement.shape)
+
+
+if __name__ == "__main__":
+    unittest.main()
