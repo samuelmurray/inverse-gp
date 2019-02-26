02acc2a5e9219d6a273403f577c64aafda9ecd4b
Samuel Murray <samuelmu@kth.se>
2019-02-26 17:56:37 +0100

    Feature: Change input of GP to be output of simple simulator

--- invgp/tests/test_gp.py
+++ invgp/tests/test_gp.py
@@ -5,17 +5,19 @@ import numpy as np
 import torch
 
 from invgp.model import GP
-from invgp.simulator import HeavySimulator
+from invgp.simulator import HeavySimulator, SimpleSimulator
 
 
 class TestExpectedImprovement(unittest.TestCase):
     def setUp(self) -> None:
         np.random.seed(1534315123)
+        simple_simulator = SimpleSimulator()
         heavy_simulator = HeavySimulator()
         self.x = torch.Tensor(np.random.normal(size=[10, 2]))
+        self.input = simple_simulator(self.x)
         self.y = heavy_simulator(self.x)
         self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self.model = GP(self.x, self.y, self.likelihood)
+        self.model = GP(self.input, self.y, self.likelihood)
 
     def test_get_inputs(self) -> None:
         model_inputs = self.model.get_inputs()
