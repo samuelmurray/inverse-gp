6bb156f463f90e653721aac2b0736cd3c6a437f4
Samuel Murray <samuelmu@kth.se>
2019-02-26 12:50:49 +0100

    Feature: Change from using __call__ to instead use forward function of gpytorch.module

--- inverse-gp/heavy_simulator.py
+++ inverse-gp/heavy_simulator.py
@@ -8,5 +8,5 @@ class HeavySimulator(Simulator):
     def __init__(self) -> None:
         super().__init__()
 
-    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.sin(x * (2 * np.pi)) + torch.randn(x.size()) * 0.2
--- inverse-gp/simple_simulator.py
+++ inverse-gp/simple_simulator.py
@@ -7,5 +7,5 @@ class SimpleSimulator(Simulator):
     def __init__(self) -> None:
         super().__init__()
 
-    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return x
