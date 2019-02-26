c3aa65d1a8a8cbb411851d9f5337e2267704bda8
Samuel Murray <samuelmu@kth.se>
2019-02-26 18:08:01 +0100

    Feature: Remove usage of SimpleSimulator; standard GP takes any input

--- invgp/tests/test_gp.py
+++ invgp/tests/test_gp.py
@@ -5,23 +5,21 @@ import numpy as np
 import torch
 
 from invgp.model import GP
-from invgp.simulator import HeavySimulator, SimpleSimulator
+from invgp.simulator import HeavySimulator
 
 
 class TestExpectedImprovement(unittest.TestCase):
     def setUp(self) -> None:
         np.random.seed(1534315123)
-        simple_simulator = SimpleSimulator()
         heavy_simulator = HeavySimulator()
         self.x = torch.Tensor(np.random.normal(size=[10, 2]))
-        self.input_train = simple_simulator(self.x)
         self.output_train = heavy_simulator(self.x)
         self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self.model = GP(self.input_train, self.output_train, self.likelihood)
+        self.model = GP(self.x, self.output_train, self.likelihood)
 
     def test_get_inputs(self) -> None:
         model_inputs = self.model.get_inputs()
-        self.assertTrue(torch.equal(self.input_train, model_inputs))
+        self.assertTrue(torch.equal(self.x, model_inputs))
 
 
 if __name__ == "__main__":
