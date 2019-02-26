5a685bc5dc0422b1842abaa1ce7be9f8ba229cad
Samuel Murray <samuelmu@kth.se>
2019-02-26 13:51:50 +0100

    Feature: Prevent instancing by subclassing abc.ABC and making forward() an abstract method

--- inverse-gp/simulators/simulator.py
+++ inverse-gp/simulators/simulator.py
@@ -1,10 +1,13 @@
+import abc
+
 import gpytorch
 import torch
 
 
-class Simulator(gpytorch.Module):
+class Simulator(gpytorch.Module, abc.ABC):
     def __init__(self) -> None:
         super().__init__()
 
+    @abc.abstractmethod
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         raise NotImplementedError
