99db3a68aa188a7bb583c49e8e39757703d26d32
Samuel Murray <samuelmu@kth.se>
2019-02-27 16:21:16 +0100

    Feature: Expose class AcquisitionFunction from package

--- invgp/acquisition_function/__init__.py
+++ invgp/acquisition_function/__init__.py
@@ -3,7 +3,9 @@ Package for all acquisition functions
 """
 
 __all__ = [
+    "AcquisitionFunction",
     "ExpectedImprovement",
 ]
 
+from .acquisition_function import AcquisitionFunction
 from .expected_improvement import ExpectedImprovement
