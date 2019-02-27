5952a6e6faca1eab78f74889ca94d1c0144cea85
Samuel Murray <samuelmu@kth.se>
2019-02-27 16:40:23 +0100

    Feature: Check that input is 2D

--- invgp/simulator/heavy_simulator.py
+++ invgp/simulator/heavy_simulator.py
@@ -9,4 +9,6 @@ class HeavySimulator(Simulator):
         super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if x.dim() != 2:
+            raise ValueError(f"Input x should be a 2D tensor, but is {x.dim()}D")
         return torch.sum(torch.sin(x * (2 * np.pi)) + torch.randn(x.size()) * 0.2, dim=(1,))
--- invgp/simulator/simple_simulator.py
+++ invgp/simulator/simple_simulator.py
@@ -8,4 +8,6 @@ class SimpleSimulator(Simulator):
         super().__init__()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if x.dim() != 2:
+            raise ValueError(f"Input x should be a 2D tensor, but is {x.dim()}D")
         return x
