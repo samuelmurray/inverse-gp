ca3d6e2c8d239ec7c25528a4af5d3f4e06baf606
Samuel Murray <samuelmu@kth.se>
2019-02-27 17:31:06 +0100

    Feature: Test compatibility with Python 3.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = "Framework for solving inverse problems using GPs"
 URL = "https://github.com/samuelmurray/inverse-gp"
 EMAIL = "samuelmu@kth.se"
 AUTHOR = "Samuel Murray"
-REQUIRES_PYTHON = ">=3.7"
+REQUIRES_PYTHON = ">=3.6"
 LICENSE = "GNU General Public License v3.0"
 TEST_DIR = "tests"
 
@@ -48,6 +48,7 @@ setup(
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
 )
