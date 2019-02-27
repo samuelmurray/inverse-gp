9e831e10d4e3e2ef9b61b5b2a517ab05944ff3ef
Samuel Murray <samuelmu@kth.se>
2019-02-27 16:02:59 +0100

    Refactor: Move class TestSimpleSimulator to separate file

---
+++ invgp/tests/test_simple_simulator.py
@@ -0,0 +1,35 @@
+import unittest
+
+import numpy as np
+import torch
+
+from invgp.simulator import SimpleSimulator
+
+
+class TestSimpleSimulator(unittest.TestCase):
+    """
+    Tests for simulator.simple_simulator.py
+    """
+
+    def setUp(self):
+        """
+        Reset numpy random seed
+        :return:
+        """
+        np.random.seed(1534315123)
+
+    def test_call(self) -> None:
+        """
+        Returned Tensor should be as long as number of datapoints
+        :return:
+        """
+        num_data = 10
+        num_dim = 2
+        simulator = SimpleSimulator()
+        x = torch.Tensor(np.random.normal(size=[num_data, num_dim]))
+        y = simulator(x)
+        self.assertEqual(num_data, y.shape[0])
+
+
+if __name__ == "__main__":
+    unittest.main()
