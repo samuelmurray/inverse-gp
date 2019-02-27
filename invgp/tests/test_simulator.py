0b1a73b3f39709e61091d25989802bbcc2fb6d21
Samuel Murray <samuelmu@kth.se>
2019-02-27 16:01:59 +0100

    Refactor: Move class TestHeavySimulator to separate file

--- invgp/tests/test_simulator.py
+++ invgp/tests/test_simulator.py
@@ -19,31 +19,6 @@ class TestSimulator(unittest.TestCase):
         self.assertRaises(TypeError, Simulator)
 
 
-class TestHeavySimulator(unittest.TestCase):
-    """
-    Tests for simulator.heavy_simulator.py
-    """
-
-    def setUp(self):
-        """
-        Reset numpy random seed
-        :return:
-        """
-        np.random.seed(1534315123)
-
-    def test_call(self) -> None:
-        """
-        Returned Tensor should be as long as number of datapoints
-        :return:
-        """
-        num_data = 10
-        num_dim = 2
-        simulator = HeavySimulator()
-        x = torch.Tensor(np.random.normal(size=[num_data, num_dim]))
-        y = simulator(x)
-        self.assertEqual(num_data, y.shape[0])
-
-
 class TestSimpleSimulator(unittest.TestCase):
     """
     Tests for simulator.simple_simulator.py
