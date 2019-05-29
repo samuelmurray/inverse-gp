0d10ddd042533f1397eca0f4979762fde3e96f18
Samuel Murray <samuelmu@kth.se>
2019-05-29 10:47:10 +0200

    Attempt at implementing simulator with code from fluid dynamics. Not working properly.

--- invgp/simulator/__init__.py
+++ invgp/simulator/__init__.py
@@ -3,11 +3,13 @@ Package for all simulator
 """
 
 __all__ = [
+    "FlowSimulator",
     "HeavySimulator",
     "SimpleSimulator",
     "Simulator",
 ]
 
+from .flow_simulator import FlowSimulator
 from .heavy_simulator import HeavySimulator
 from .simple_simulator import SimpleSimulator
 from .simulator import Simulator
---
+++ invgp/simulator/flow_simulator.py
@@ -0,0 +1,51 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+
+from .simulator import Simulator
+
+
+class FlowSimulator(Simulator):
+    def __init__(self, dt: float, nt: int) -> None:
+        super().__init__()
+        self.dt = dt
+        self.nt = nt
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x_as_ndarray = x.numpy()
+        _, fwd = self.rk4_solver_fwd(x_as_ndarray)
+        norm = fwd[-1, 0] ** 2 + fwd[-1, 1] ** 2
+        print(norm)
+        fwd_as_tensor = torch.tensor([norm])
+        print(fwd_as_tensor.shape)
+        return fwd_as_tensor
+
+    @staticmethod
+    def rhs_fwd(x: np.ndarray) -> np.ndarray:
+        """ RHS of the forward problem: dX/dt=f(X(t))
+        :param x: d-by-1 vector, solution variable
+        :param t: scalar, time
+        """
+        rhs = np.zeros(len(x))
+        rhs[0] = -x[0] + 10.0 * x[1]
+        rhs[1] = x[1] * (10.0 * np.exp(-x[0] * x[0] / 100.0) - x[1]) * (x[1] - 1.0)
+        return rhs
+
+    def rk4_solver_fwd(self, x0: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """ RK4 solver for the forward problem
+        :param x0: N-by-d vector, initial condition
+        """
+        t = np.zeros(self.nt)
+        d = x0.shape[1]
+        x = np.zeros((self.nt, d))
+        x[0, :] = x0[:, 0]
+        for i in range(1, self.nt):
+            im = i - 1
+            k1 = self.rhs_fwd(x[im, :])
+            k2 = self.rhs_fwd(x[im, :] + 0.5 * self.dt * k1)
+            k3 = self.rhs_fwd(x[im, :] + 0.5 * self.dt * k2)
+            k4 = self.rhs_fwd(x[im, :] + self.dt * k3 + self.dt * k1)
+            x[i, :] = x[im, :] + (self.dt / 6.0) * (k1 + 2. * k2 + 2. * k3 + k4)
+            t[i] = t[im] + self.dt
+        return t, x
